71c17f71a0c1eca5eb2abfa8d9e5f3f9beccb385
elfadili-ae <elfadili.ae@gmail.com>
2024-01-03 19:06:56 +0000

    promise and settled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((responses) => responses.map((response) => (
+      {
+        status: response.status,
+        value: response.status === 'fulfilled' ? response.value : String(response.reason),
+      }
+    )));
+}
