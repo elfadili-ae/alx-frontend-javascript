a5347e2eaf972d39bb2e73512c13abed2f9b6188
elfadili-ae <elfadili.ae@gmail.com>
2024-01-03 21:46:10 +0000

    async await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return {
+      photo,
+      user,
+    };
+  } catch (_) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+};
+
+export default asyncUploadUser;
