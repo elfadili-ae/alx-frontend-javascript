7c3ac5995ecaa9fb68124c0e03b5a222ec82795e
elfadili-ae <elfadili.ae@gmail.com>
2024-01-03 14:32:25 +0000

    Fix: added underscore

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
+  return new Promise((_, reject) => {
     reject(new Error(`${filename} cannot be processed`));
   });
 }
