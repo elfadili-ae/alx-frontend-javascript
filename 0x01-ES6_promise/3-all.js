e871588bebe4423357e5c918319a5223e8be90b9
elfadili-ae <elfadili.ae@gmail.com>
2024-01-03 12:39:17 +0000

    Fix: return promise resolution

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const user = createUser();
-  const photo = uploadPhoto();
-
-  Promise.all([user, photo])
+  return Promise.all([createUser(), uploadPhoto()])
     .then(([userResp, PhotoResp]) => {
       console.log(`${PhotoResp.body} ${userResp.firstName} ${userResp.lastName}`);
     })
