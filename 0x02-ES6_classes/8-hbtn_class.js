c1d080cf7fc9cb224622bdbf8dea953d2bcc8395
elfadili-ae <elfadili.ae@gmail.com>
2024-01-04 19:26:28 +0000

    Customize object conversion

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(val) {
+    this._size = val;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(val) {
+    this._location = val;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    }
+
+    if (hint === 'number') {
+      return this._size;
+    }
+  }
+}
