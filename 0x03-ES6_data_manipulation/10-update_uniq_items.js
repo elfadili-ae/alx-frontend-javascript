2c6f71015e830569288830f0b2e9c514175bbdaa
elfadili-ae <elfadili.ae@gmail.com>
2024-01-08 18:04:15 +0000

    check if arg is a map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(mapu) {
+  if (!(mapu instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   for (const [key, value] of mapu) {
     if (value === 1) {
       mapu.set(key, 100);
