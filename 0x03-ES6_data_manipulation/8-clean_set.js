5d24b8511855a24c3e67498d082720ef30f4ab2e
elfadili-ae <elfadili.ae@gmail.com>
2024-01-12 23:15:23 +0000

    new approach

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,12 @@
 export default function cleanSet(mySet, startString) {
-  const res = [];
-  if (startString && startString !== '') {
-    for (const element of mySet) {
-      if (typeof element !== 'string') {
-        return '';
-      }
-      if (element && element.startsWith(startString)) {
-        res.push(element.slice(startString.length));
-      }
+  let res = '';
+  if (!startString || !startString.length) {
+    return '';
+  }
+  for (const element of mySet) {
+    if (element && element.startsWith(startString)) {
+      res += `${element.slice(startString.length)}-`;
     }
   }
-  return res.join('-');
+  return res.slice(0, -1);
 }
