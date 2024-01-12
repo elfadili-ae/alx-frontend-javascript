28daec57ba01c8580320fb8d8d23271bb56be8e2
elfadili-ae <elfadili.ae@gmail.com>
2024-01-12 23:08:55 +0000

    check element if string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,14 @@
 export default function cleanSet(mySet, startString) {
   const res = [];
   if (startString && startString !== '') {
-    mySet.forEach((element) => {
+    for (const element of mySet) {
+      if (typeof element !== 'string') {
+        return '';
+      }
       if (element && element.startsWith(startString)) {
         res.push(element.slice(startString.length));
       }
-    });
+    }
   }
   return res.join('-');
 }
